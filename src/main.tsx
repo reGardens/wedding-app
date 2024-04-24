a81a8b3f8e3025f642573cdd7f1da6fa1a134c47
Petani Kode <rezzabagus.rb@gmail.com>
2024-04-24 10:54:57 +0700

    fix: fixing query param url

--- src/main.tsx
+++ src/main.tsx
@@ -3,22 +3,38 @@ import ReactDOM from 'react-dom/client'
 import App from './pages/App.tsx'
 import {
   createBrowserRouter,
+  createRoutesFromElements,
+  Route,
   RouterProvider,
 } from "react-router-dom";
 import ErrorPage from "./error-page.tsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/:slug",
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-]);
+const router = createBrowserRouter(
+  // createRoutesFromElements(
+  //   <Route
+  //   path="teams/:teamId"
+  // the matching param will be available to the loader
+  // loader={({ params }) => {
+  //   console.log(params.teamId); // "hotspur"
+  // }}
+  // and the action
+  //   action={({ params }) => { }}
+  //   errorElement={<ErrorBoundary />}
+  // element={<App />}
+  // />
+  // )
+  [
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/:teamId",
+      element: <App />,
+    },
+  ]
+);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,19 +2,27 @@ import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 export default function Home() {
-  const { slug } = useParams<{ slug: string }>();
+  // example 1
+  // const { slug } = useParams<{ slug: string }>();
+  // useEffect(() => {
+  //   if (slug) {
+  //     getPost(slug);
+  //   }
+  // }, [slug]);
+  // const getPost = (slug: string) => {
+  //   console.log(slug);
+  // };
 
-  useEffect(() => {
-    if (slug) {
-      getPost(slug);
-    }
-  }, [slug]);
-  
-  const getPost = (slug: string) => {
-    console.log(slug);
-  };
+  // example 2
+  const queryParameters = new URLSearchParams(window.location.search)
+  const type = queryParameters.get("type")
+  const name = queryParameters.get("name")
 
-  return(
-    <p className="text-red-500">Selamat Datang {slug}</p>
+  return (
+    <>
+      {/* <p className="text-red-500">Selamat Datang {slug}</p> */}
+      <p>Type: {type}</p>
+      <p>Name: {name}</p>
+    </>
   )
 }
