5d3ba03c61cc3556fce31a6221e998a2789abfd9
Petani Kode <rezzabagus.rb@gmail.com>
2024-04-25 11:04:11 +0700

    fix:fixing coverletter

--- src/components/CoverLetter/index.tsx
+++ src/components/CoverLetter/index.tsx
@@ -2,7 +2,6 @@ import Layout from "../Layout";
 import dummyAvatar from "../../assets/image/avatar.webp"
 import { Avatar, Button } from "@mui/material"
 import gsap from "gsap";
-// import backgroundDummy from "../../assets/image/background-dummy.webp"
 
 export default function CoverLetter() {
     // get query parameter url
@@ -24,29 +23,22 @@ export default function CoverLetter() {
             <div className="cover max-w-screen-sm h-screen absolute top-0 left-0 z-[999]">
                 <img src={dummyAvatar} alt="" loading="lazy" srcSet="" className="max-w-screen-sm h-screen absolute top-0 left-0 w-full object-cover" />
                 <div className="cover max-w-screen-sm h-screen mx-auto text-white dark:text-white text-center overflow-y-auto backdrop-blur-xl bg-white/30 dark:bg-black/30 p-4 lg:p-10">
-                    <div className="mb-10">
+                    <p className="font-bold lg:text-lg mb-5">Undangan Pernikahan</p>
+                    <div className="">
                         <Avatar
                             alt="Remy Sharp"
                             src={dummyAvatar}
                             sx={{ width: 200, height: 200, margin: '0 auto' }}
                         />
                     </div>
-                    <p className="font-bold lg:text-lg">Pernikahan</p>
-                    <p className="font-bold text-3xl lg:text-4xl font-greatVibes tracking-widest py-1">Personalia & Personalia</p>
+                    <p className="font-bold text-3xl lg:text-4xl font-greatVibes tracking-widest pt-5 pb-1">Personalia & Personalia</p>
+                    <p className="font-bold text-3xl lg:text-4xl font-greatVibes tracking-widest pb-5">03. 05. 1997</p>
                     <p>Dengan rasa syukur kepada Tuhan Yang Maha Esa,<br /> kami dengan bahagia mengundang Anda untuk berbagi kebahagiaan kami dalam upacara pernikahan kami.</p>
 
-                    <div className="border rounded-lg py-5 mt-4 mb-5">
+                    <div className="border rounded-lg py-3 mt-4 mb-5">
                         <p className="font-bold text-lg lg:text-xl uppercase py-1">{to} & {and}</p>
-                        <p className="font-bold text-3xl lg:text-4xl font-greatVibes tracking-widest pb-1">03. 05. 1997</p>
                         <p>Di Tempat: </p>
                     </div>
-
-                    {/* <div className="grid grid-cols-7 text-left">
-                    <p className="col-span-1">Lokasi:</p>
-                    <p className="col-span-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate <br />
-                    <Button variant="contained" className="!font-bold">Klik Untuk Lihat Lokasi</Button>
-                    </p>
-                </div> */}
 
                     <Button
                         variant="contained"
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
 import CoverLetter from "../../components/CoverLetter"
 import Layout from "../../components/Layout"
 import dummyAvatar from "../../assets/image/avatar.webp"
-// import DateCountdown from 'react-date-countdown-timer';
+import DateCountdown from 'react-date-countdown-timer';
 
 export default function Home() {
   // let DateCountdown: any;
@@ -14,7 +14,7 @@ export default function Home() {
         </div>
         <div className="text-center py-4 lg:py-10 absolute bottom-0 w-full grid justify-center z-10">
           <p className="drop-shadow-2xl font-extrabold text-4xl lg:text-6xl font-greatVibes tracking-widest relative">Personalia <br /> & <br /> Personalia</p> <br />
-          {/* <DateCountdown dateTo='January 01, 2025 00:00:00 GMT+07:00' callback={() => alert('Datang Ya')} /> */}
+          <DateCountdown dateTo='January 01, 2025 00:00:00 GMT+07:00' callback={() => alert('Datang Ya')} />
         </div>
       </main>
     </Layout>
