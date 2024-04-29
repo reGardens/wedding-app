50bffb89258afcfce2986429565602d5157c2bbb
Petani Kode <rezzabagus.rb@gmail.com>
2024-04-29 15:42:02 +0700

    feat: section doa && section the bride

--- src/components/CoverLetter/index.tsx
+++ src/components/CoverLetter/index.tsx
@@ -6,16 +6,16 @@ import { useEffect, useMemo, useState } from "react";
 import gifMusic from '/gif/music.gif'
 import gifPause from '/icon/pause-button.webp'
 
-// interface MusicProps {
-//     url: string;
-// }
-
 export default function CoverLetter() {
     // get query parameter url
     const queryParameters = new URLSearchParams(window.location.search)
     const to = queryParameters.get("to")
     const and = queryParameters.get("and")
 
+    // modal
+    const [showModal, setShowModal] = useState<boolean>(true);
+
+    // audio
     const [play, setPlay] = useState(false)
     const audio = useMemo(() => {
         return new Audio('/Mp3/test.mp3');
@@ -23,13 +23,26 @@ export default function CoverLetter() {
 
     useEffect(() => {
         localStorage.setItem('play', JSON.stringify(play));
-    }, [play]);
+
+        if (showModal) {
+            document.body.style.overflow = "hidden"
+        } else {
+            document.body.style.overflow = "auto"
+        }
+    }, [play, showModal]);
 
     const handleCoverLetter = () => {
-        // handle animation
+        // handle modal animation
         gsap.to(".cover", {
             rotation: -50,
             x: '-100%',
+            duration: 1,
+            opacity: 0,
+            display: 'none'
+        });
+        gsap.to(".modal", {
+            // rotation: -50,
+            // x: '-100%',
             duration: 1,
             opacity: 0,
             display: 'none'
@@ -50,58 +63,68 @@ export default function CoverLetter() {
         } else {
             audio.pause();
         }
+
+        setShowModal(false)
     }
 
     return (
         <Layout>
-            <div className="cover max-w-screen-sm h-screen absolute top-0 left-0 z-[999]">
-                <img src={dummyAvatar} alt="" loading="lazy" srcSet="" className="max-w-screen-sm h-screen absolute top-0 left-0 w-full object-cover" />
-                <div className="cover max-w-screen-sm h-screen mx-auto text-white dark:text-white text-center overflow-y-auto backdrop-blur-xl bg-white/30 dark:bg-black/30 p-4 lg:p-10">
-                    <p className="font-bold lg:text-lg mb-5 tracking-widest uppercase">Undangan Pernikahan</p>
-                    <div className="">
-                        <Avatar
-                            alt="Remy Sharp"
-                            src={dummyAvatar}
-                            sx={{ width: 200, height: 200, margin: '0 auto' }}
-                        />
-                    </div>
-                    <p className="font-bold text-3xl lg:text-4xl font-greatVibes tracking-widest pt-5 pb-1">Personalia & Personalia</p>
-                    <p className="font-bold text-3xl lg:text-4xl font-greatVibes tracking-widest pb-5">03. 05. 1997</p>
-                    <p>Dengan rasa syukur kepada Tuhan Yang Maha Esa,<br /> kami dengan bahagia mengundang Anda untuk berbagi kebahagiaan kami dalam upacara pernikahan kami.</p>
+            <div className="modal w-full h-full top-0 left-0 mx-auto flex justify-center z-[999] overflow-hidden fixed">
+                <div className="cover max-w-screen-sm h-screen mx-auto relative z-[998]">
+                    <img src={dummyAvatar} alt="" loading="lazy" srcSet="" className="h-screen absolute top-0 left-0 w-full object-cover mx-auto" />
+                    <div className="cover flex flex-col justify-start sm:justify-center max-w-screen-sm h-screen mx-auto text-white dark:text-white text-center overflow-y-auto backdrop-blur-xl bg-white/30 dark:bg-black/30 p-4 lg:p-10">
+                        <p className="font-bold lg:text-lg mb-5 tracking-widest uppercase">Undangan Pernikahan</p>
+                        <div className="">
+                            <Avatar
+                                alt="Remy Sharp"
+                                src={dummyAvatar}
+                                sx={{ width: 200, height: 200, margin: '0 auto' }}
+                            />
+                        </div>
+                        <p className="font-bold text-3xl lg:text-4xl font-greatVibes tracking-widest pt-5 pb-1">Personalia & Personalia</p>
+                        <p className="font-bold text-3xl lg:text-4xl font-greatVibes tracking-widest pb-5">03. 05. 1997</p>
+                        <p>Dengan rasa syukur kepada Tuhan Yang Maha Esa,<br /> kami dengan bahagia mengundang Anda untuk berbagi kebahagiaan kami dalam upacara pernikahan kami.</p>
 
-                    <div className="border rounded-lg py-3 mt-4 mb-5">
-                        <p className="font-bold text-lg lg:text-xl uppercase py-1">{to} & {and}</p>
-                        <p>Di Tempat: </p>
-                    </div>
+                        <div className="border rounded-lg py-3 mt-4 mb-5">
+                            <p className="font-bold text-lg lg:text-xl uppercase py-1">{to} & {and}</p>
+                            <p>Di Tempat: </p>
+                        </div>
 
-                    <Button
-                        variant="contained"
-                        sx={{
-                            fontWeight: 'bold'
-                        }}
-                        onClick={handleCoverLetter}
-                    >
-                        Buka Undangan
-                    </Button>
+                        <Button
+                            variant="contained"
+                            sx={{
+                                fontWeight: 'bold',
+                                width: 170,
+                                marginX: 'auto',
+                            }}
+                            onClick={handleCoverLetter}
+                        >
+                            Buka Undangan
+                        </Button>
+                    </div>
                 </div>
             </div>
 
             {/* play button music */}
-            {localStorage.getItem("play") !== null && localStorage.getItem("play") === "true" ?
-                <Button
-                    className="ml-[10px] md:!ml-[15px]"
-                    onClick={handleCoverLetter}
-                >
-                    <img src={gifMusic} alt="" className="fixed top-1 z-[888] w-14 md:w-20 md:h-20 object-contain drop-shadow-2xl" />
-                </Button>
-                :
-                <Button
-                    className="ml-[10px] md:!ml-[15px]"
-                    onClick={handleCoverLetter}
-                >
-                    <img src={gifPause} alt="" className="fixed top-1 z-[888] w-14 md:w-16 md:h-20 object-contain drop-shadow-2xl" />
-                </Button>
-            }
+            <div className="fixed w-full mx-auto z-[997]">
+                <div className="max-w-screen-sm mx-auto">
+                    {localStorage.getItem("play") !== null && localStorage.getItem("play") === "true" ?
+                        <Button
+                            className=""
+                            onClick={handleCoverLetter}
+                        >
+                            <img src={gifMusic} alt="" className="w-14 md:w-16 md:h-20 object-contain drop-shadow-2xl" />
+                        </Button>
+                        :
+                        <Button
+                            className=""
+                            onClick={handleCoverLetter}
+                        >
+                            <img src={gifPause} alt="" className="w-14 md:w-16 md:h-20 object-contain drop-shadow-2xl" />
+                        </Button>
+                    }
+                </div>
+            </div>
         </Layout>
     )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,8 +2,23 @@ import CoverLetter from "../../components/CoverLetter"
 import Layout from "../../components/Layout"
 import dummyAvatar from "../../assets/image/avatar.webp"
 import Countdown, { CountdownRenderProps } from 'react-countdown';
+import { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import bgTheBride from "/Image/bg-about-black.jpeg";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import defaultImage from "/Default/default-image.png";
+import { Link } from "react-router-dom";
+import { Divider } from "@mui/material";
 
 export default function Home() {
+  gsap.registerPlugin(ScrollTrigger)
+
   // set target time
   const countDownTimer = new Date('2025-04-26T00:00:00');
   // start custom style date
@@ -16,25 +31,38 @@ export default function Home() {
       const formattedDays = days < 10 ? `0${days}` : days;
       // Render the countdown with custom format
       return (
-        <div className="grid grid-cols-4 backdrop-blur-xl bg-white/30 dark:bg-black/30 py-2 rounded-lg">
-          <div><span className="text-4xl">{formattedDays}</span> <br /> <span>Hari</span></div>
-          <div><span className="text-4xl">{hours}</span> <br /> <span>Jam</span></div>
-          <div><span className="text-4xl">{minutes}</span> <br /> <span>Menit</span></div>
-          <div><span className="text-4xl">{seconds}</span> <br /> <span>Detik</span></div>
+        <div className="grid grid-cols-4 gap-3 backdrop-blur-xl bg-white/30 dark:bg-black/30 py-2 px-4 rounded-lg">
+          <div><span className="text-2xl md:text-3xl">{formattedDays}</span> <br /> <span>Hari</span></div>
+          <div><span className="text-2xl md:text-3xl">{hours}</span> <br /> <span>Jam</span></div>
+          <div><span className="text-2xl md:text-3xl">{minutes}</span> <br /> <span>Menit</span></div>
+          <div><span className="text-2xl md:text-3xl">{seconds}</span> <br /> <span>Detik</span></div>
         </div>
       );
     }
   };
 
+  useEffect(() => {
+    const doa = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.doa',
+        start: "top center",
+        end: "bottom center",
+        toggleActions: "play none none reverse",
+      }
+    });
+    doa.to(".doa div", { y: 0, duration: 1.8, ease: "back.out(1.1)", opacity: 1 });
+  }, []);
+
   return (
     <Layout>
+      <CoverLetter />
+
       <main className="bg-header max-w-screen-sm h-screen mx-auto relative overflow-hidden">
-        <CoverLetter />
         <div className="w-full h-full absolute top-0 left-0">
           <img src={dummyAvatar} alt="" className="w-full h-full object-cover" />
         </div>
         <div className="text-center py-4 lg:py-10 absolute bottom-0 w-full grid justify-center z-10">
-          <p className="drop-shadow-2xl font-extrabold text-4xl lg:text-6xl font-greatVibes tracking-widest relative">Personalia <br /> & <br /> Personalia</p> <br />
+          <p className="drop-shadow-2xl font-extrabold text-4xl lg:text-5xl font-greatVibes tracking-widest relative">Personalia <br /> & <br /> Personalia</p> <br />
           <Countdown
             date={countDownTimer}
             renderer={renderer}
@@ -42,6 +70,71 @@ export default function Home() {
           </Countdown>
         </div>
       </main>
+
+      <section className="doa text-center max-w-screen-sm mx-auto relative overflow-hidden backdrop-blur-md bg-black/30 py-4 lg:py-10 px-4">
+        <div className="translate-y-10 opacity-0">
+          <p className="text-lg mb-3"> QS. Ar-Rum Ayat 21 </p>
+          <p className="">
+            <span className="text-xl py-5">وَمِنْ اٰيٰتِهٖٓ اَنْ خَلَقَ لَكُمْ مِّنْ اَنْفُسِكُمْ اَزْوَاجًا لِّتَسْكُنُوْٓا اِلَيْهَا وَجَعَلَ بَيْنَكُمْ مَّوَدَّةً وَّرَحْمَةً ۗاِنَّ فِيْ ذٰلِكَ لَاٰيٰتٍ لِّقَوْمٍ يَّتَفَكَّرُوْنَ</span> <br />
+            <span className="text-base"> "Dan di antara tanda-tanda (kebesaran)-Nya adalah Dia menciptakan sekutu bagimu dari jenismu sendiri, agar kamu condong dan merasa tenteram terhadapnya, dan Dia menciptakan di antara kamu perasaan cinta dan kasih sayang. Sesungguhnya pada yang demikian itu benar-benar terdapat tanda-tanda (kebesaran Allah) bagi orang-orang yang berpikir."</span>
+          </p>
+        </div>
+      </section>
+
+      <section className="max-w-screen-sm mx-auto relative overflow-hidden py-4 lg:py-10 px-4">
+        <img src={bgTheBride} alt="" srcSet="" className="absolute top-0 left-0 w-full h-full object-cover opacity-75 -z-10" />
+        <p className="font-bold text-3xl lg:text-4xl tracking-widest pb-5">THE BRIDE</p>
+
+        <Card sx={{}} className="!backdrop-blur-md !bg-[#32CD32]/30">
+          <CardMedia
+            sx={{ height: 340 }}
+            image={defaultImage}
+            title="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div" className="!font-greatVibes !tracking-[5px] !text-4xl">
+              Personalia
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              PUTRO PERTAMA DARI:
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Bpk. ............... dan Ibu. ............... <br />
+              Alamat ...............
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">
+              <Link to="test">Instagram</Link>
+            </Button>
+          </CardActions>
+        </Card>
+        <Divider orientation="vertical" variant="middle" flexItem />
+        <Card sx={{}} className="!backdrop-blur-md !bg-[#800080]/30">
+          <CardMedia
+            sx={{ height: 340 }}
+            image={defaultImage}
+            title="green iguana"
+          />
+          <CardContent sx={{ textAlign: 'right' }}>
+            <Typography gutterBottom variant="h5" component="div" className="!font-greatVibes !tracking-[5px] !text-4xl">
+              Personalia
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              PUTRO PERTAMA DARI:
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Bpk. ............... dan Ibu. ............... <br />
+              Alamat ...............
+            </Typography>
+          </CardContent>
+          <CardActions sx={{ float: 'right' }}>
+            <Button size="small">
+              <Link to="test">Instagram</Link>
+            </Button>
+          </CardActions>
+        </Card>
+      </section>
     </Layout>
   )
 }
